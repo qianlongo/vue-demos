2b6150b77155e45ea667685fefccc25f30707749
qianlongo <1162633511@qq.com>
2021-11-05 00:09:52 +0800

    feat: prop

---
+++ src/views/modifiers/camel.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="camel">
+    <svg :viewBox="viewbox"></svg>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      viewbox: '0 0 100 100'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/modifiers/prop.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="prop">
+    <div class="prop-item" :my-name="prop" ref="prop"></div>
+    <div class="prop-item" :my-name.prop="prop2" ref="prop2"></div>
+
+    <button @click="onGetResult">获取结果</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'prop',
+  data () {
+    return {
+      prop: 'hello prop',
+      prop2: 'hello prop2'
+    }
+  },
+  methods: {
+    onGetResult () {
+      const $refProp = this.$refs.prop
+      const $refProp2 = this.$refs.prop2
+
+      console.log($refProp.getAttribute('my-name'))
+      console.log($refProp2.getAttribute('my-name'))
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
