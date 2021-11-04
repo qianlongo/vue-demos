5097fd4f7055eee4c84b7abdcbad32c2277cd8db
qianlongo <1162633511@qq.com>
2021-11-05 02:14:04 +0800

    feat: 构建

--- src/views/modifiers/custom.vue
+++ src/views/modifiers/custom.vue
@@ -1,25 +1,15 @@
 <template>
   <div class="custom">
-    <!-- 加上.native之后原生事件才得以监听成功 -->
-    <NativeCustom @onKeydown="onKeydown" @click="onClick" />
+    <input type="text" @keydown.q="f1Keydown">
   </div>
 </template>
 
 <script>
-
-import NativeCustom from '../../components/native.vue'
-
 export default {
   name: 'custom',
-  components: {
-    NativeCustom
-  },
   methods: {
-    onKeydown () {
-      console.log('onKeydown')
-    },
-    onClick () {
-      console.log('onClick')
+    f1Keydown () {
+      console.log('按下了q')
     }
   }
 }
--- src/views/modifiers/native.vue
+++ src/views/modifiers/native.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="native"></div>
+  <div class="custom">
+    <!-- 加上.native之后原生事件才得以监听成功 -->
+    <NativeCustom @onKeydown="onKeydown" @click.native="onClick" />
+  </div>
 </template>
 
 <script>
-export default {
 
+import NativeCustom from '../../components/native.vue'
+
+export default {
+  name: 'custom',
+  components: {
+    NativeCustom
+  },
+  methods: {
+    onKeydown () {
+      console.log('onKeydown')
+    },
+    onClick () {
+      console.log('onClick')
+    }
+  }
 }
 </script>
 
