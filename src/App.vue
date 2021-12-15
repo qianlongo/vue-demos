8da76d8e39fcf39ca25829aae854f1ef9b190b52
qianlongo <1162633511@qq.com>
2021-12-15 22:50:26 +0800

    feat: v-if示例

--- src/App.vue
+++ src/App.vue
@@ -6,30 +6,7 @@
 
 <style>
 body{
-  display: flex;
-  justify-content: center;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  max-width: 375px;
-  padding: 20px 0;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  padding: 20px;
 }
 
 *{
---
+++ src/views/directives/v-if.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="v-if">
+    <button @click="onToggleShow">切换</button>
+    <div v-if="isShow">前端胖头鱼 显示出来啦</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vif',
+  data () {
+    return {
+      isShow: true
+    }
+  },
+  methods: {
+    onToggleShow () {
+      this.isShow = !this.isShow
+    }
+  }
+}
+</script>
+
+<style scoped>
+button{
+  display: block;
+  padding:0 10px;
+}
+</style>
