a61d83a164d3fe5b4cc689357eb4dbb68205bf4a
qianlongo <1162633511@qq.com>
2021-12-15 23:14:00 +0800

    feat: 添加v-show

---
+++ src/views/directives/v-show.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="v-show">
+    <button @click="onToggleShow">切换</button>
+    <div v-show="isShow">前端胖头鱼 显示出来啦</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vshow',
+  data () {
+    return {
+      isShow: true
+    }
+  },
+  methods: {
+    onToggleShow () {
+      this.isShow = !this.isShow
+    }
+  }
+}
+</script>
