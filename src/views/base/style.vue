5b116f95afd8c299f26c0df16c10f4425645e360
qianlongo <1162633511@qq.com>
2021-12-19 22:04:24 +0800

    feat: 添加style例子

---
+++ src/views/base/style.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="style" :style="[ style, style2 ]"></div>
+</template>
+
+<script>
+export default {
+  name: 'style',
+  data () {
+    return {
+      style: {
+        width: '100%',
+        height: '500px',
+      },
+      style2: {
+        backgroundImage: 'linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%)',
+        borderRadius: '10px',
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
