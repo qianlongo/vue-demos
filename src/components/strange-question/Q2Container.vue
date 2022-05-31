d8d144cbfbcad44b9cd178a1b33450aa3c902364
qianlongo <1162633511@qq.com>
2022-05-31 23:08:27 +0800

    feat: 第二个例子

---
+++ src/components/strange-question/Q2Container.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="children-container" v-if="$slots.default">
+    <p>The content of children is:</p>
+    <slot></slot>
+  </div>
+  <div v-else className="empty">empty</div>
+</template>
+
+<script>
+export default {
+  name: 'Q2Container',
+  mounted () {
+    console.log(this.$slots.default)
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/strange-question/q2.vue
@@ -0,0 +1,29 @@
+<template>
+  <Q2Container>
+    <!-- 11 -->
+    <div className="name-list" v-for="(name, index) in list" :key="index">{{ name }}</div>
+  </Q2Container>
+</template>
+
+<script>
+import Q2Container from '../../components/strange-question/Q2Container'
+
+export default {
+  name: 'Q1',
+  components: {
+    Q2Container
+  },
+  data () {
+    return {
+      list: [ 'fatfish' ]
+    }
+  },
+  mounted () {
+    // console.log(this.$children)
+  }
+}
+</script>
+
+<style>
+
+</style>
