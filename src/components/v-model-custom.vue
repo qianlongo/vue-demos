3152909d9077b3d6a0e43efcdb47439f84eb1d9b
qianlongo <1162633511@qq.com>
2022-06-05 21:59:49 +0800

    feat: 自定义v-model

---
+++ src/components/v-model-custom.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="v-model-custom">
+    <input type="text" :value="value" @input="onInput">
+    <div class="v-model-custom-msg">{{ value }}</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'VModalCustom',
+  props: {
+    value: {
+      type: String,
+    }
+  },
+  methods: {
+    onInput (e) {
+      this.$emit('input', e.target.value)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/directives/v-model.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="v-model">
+    <input type="text" v-model="model1">
+    <input type="text" :value="model2" @input="onInput">
+    <VModelCustom v-model="model3" />
+  </div>
+</template>
+
+<script>
+import VModelCustom from '../../components/v-model-custom.vue'
+
+export default {
+  name: 'vif',
+  data () {
+    return {
+      model1: 'hello',
+      model2: 'hello2',
+      model3: 'hello3'
+    }
+  },
+  components: {
+    VModelCustom
+  },
+  methods: {
+    onInput (e) {
+      this.model2 = e.target.value
+      console.log(this.model2)
+    }
+  }
+}
+</script>
